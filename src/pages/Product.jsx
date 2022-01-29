2fbdb2b7d4f389e3c8beaf4697341d1f6f475028
Aashish Panthi <aashishpanthi11@gmail.com>
2022-01-29 21:49:08 +0545

    added useeffect dependency

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,10 +5,11 @@ import { useWeb3 } from "@3rdweb/hooks";
 import { NFTModule, NFTMetadataOwner } from "@3rdweb/sdk";
 import { ethers } from "ethers";
 import { useParams } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const Product = () => {
   const { id: productId } = useParams();
-  const [product, setProduct] = useState({});
+  const [products, setProducts] = useState([]);
   const { provider } = useWeb3();
 
   const getItem = async () => {
@@ -18,9 +19,8 @@ const Product = () => {
     );
 
     try {
-      const item = await module.getDirectListing(productId);
-      setProduct(item);
-      console.log(item);
+      const allitems = await module.getAllListings();
+      setProducts(allitems);
     } catch (error) {
       console.log(error);
     }
@@ -30,29 +30,37 @@ const Product = () => {
 
   useEffect(() => {
     getItem();
-  }, []);
+  }, [products]);
 
   return (
     <>
       <div className="bg-white">
-        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div>
-            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-              {product?.asset.name}
-            </h2>
-            <span>{product?.asset.description}</span>
-          </div>
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            <div className="w-full min-h-90 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-100 lg:aspect-none">
-              <img
-                src={`https://cloudflare-ipfs.com/ipfs${product?.asset.image.substring(6,product.asset.image.length)}`}
-                alt={product?.asset.name}
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
+        {products.length == 0 ? (
+          <Loader />
+        ) : (
+          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div>
+              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                {products[productId].asset.name}
+              </h2>
+              <span>{products[productId].asset.description}</span>
             </div>
-            <div></div>
+            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+              <div className="w-full min-h-90 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-100 lg:aspect-none">
+                <img
+                  src={`https://cloudflare-ipfs.com/ipfs${products[
+                    productId
+                  ].asset.image.substring(
+                    6,
+                    products[productId].asset.image.length
+                  )}`}
+                  alt={products[productId].asset.name}
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <Footer />
     </>
